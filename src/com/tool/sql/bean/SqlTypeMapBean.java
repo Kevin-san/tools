13ccbc2fe940384a2c758f613f0e035d15a1d53a
xcKev <xckevin1620@outlook.com>
2018-07-01 11:23:01 +0800

    remove license

---
+++ src/com/tool/sql/bean/SqlTypeMapBean.java
@@ -0,0 +1,40 @@
+package com.tool.sql.bean;
+
+public class SqlTypeMapBean {
+	private String sqlType;
+	private String jdbcType;
+	private String ibaitsType;
+	private String hsqlType;
+	private String javaType;
+	public String getSqlType() {
+		return sqlType;
+	}
+	public void setSqlType(String sqlType) {
+		this.sqlType = sqlType;
+	}
+	public String getJdbcType() {
+		return jdbcType;
+	}
+	public void setJdbcType(String jdbcType) {
+		this.jdbcType = jdbcType;
+	}
+	public String getIbaitsType() {
+		return ibaitsType;
+	}
+	public void setIbaitsType(String ibaitsType) {
+		this.ibaitsType = ibaitsType;
+	}
+	public String getHsqlType() {
+		return hsqlType;
+	}
+	public void setHsqlType(String hsqlType) {
+		this.hsqlType = hsqlType;
+	}
+	public String getJavaType() {
+		return javaType;
+	}
+	public void setJavaType(String javaType) {
+		this.javaType = javaType;
+	}
+	
+}
--- src/com/tool/sql/map/FileMapConstant.java
+++ src/com/tool/sql/map/FileMapConstant.java
@@ -1,5 +1,5 @@
 package com.tool.sql.map;
 
 public class FileMapConstant {
-
+	public static final String PIPE_LINE ="|";
 }
